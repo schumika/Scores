bd148f74d618bc9e4dc6d43e3373fa2413c03771
schumika <schumika@gmail.com>
2013-08-26 22:07:35 +0300

    Initial Commit

---
+++ Scores/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Scores
+//
+//  Created by Anca Calugar on 8/26/13.
+//  Copyright (c) 2013 mine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FTAppDelegate class]));
+    }
+}
---
+++ ScoresTests/ScoresTests.m
@@ -0,0 +1,36 @@
+//
+//  ScoresTests.m
+//  ScoresTests
+//
+//  Created by Anca Calugar on 8/26/13.
+//  Copyright (c) 2013 mine. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ScoresTests : XCTestCase
+
+@end
+
+@implementation ScoresTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
